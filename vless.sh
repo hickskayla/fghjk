63f9add0c1a1ddf4f66dd3dce2b936e746f2ec7d
hickskayla <111619628+hickskayla@users.noreply.github.com>
2022-10-10 19:27:52 -0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -5,7 +5,7 @@ wget -N https://raw.githubusercontent.com/hickskayla/fghjk/main/web
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="1eb6e917-774b-4a84-aff6-b058577c60a5"
+    id="d5bcb493-4c12-41b0-aace-f57577fd9a98"
 fi
 
 cat <<EOF > ~/config.json
